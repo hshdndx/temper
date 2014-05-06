8afb0202fa12237851b1ce6edb2b4fe8793aa5ec
hshdndx <hanshuang_dndx@163.com>
2014-05-06 20:52:47 +0800

    Add main.c

--- main.c
+++ main.c
@@ -112,28 +112,28 @@ __interrupt void ADC12_ISR(void)
 }
 void SendtoBluetooth()
 {
-	P3SEL = BIT3+BIT4;                        // P3.4,5 = USCI_A0 TXD/RXD
-	  UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
-	  UCA0CTL1 |= UCSSEL_2;                     // SMCLK
-	  UCA0BR0 = 6;                              // 1MHz 9600 (see User's Guide)
-	  UCA0BR1 = 0;                              // 1MHz 9600
-	  UCA0MCTL = UCBRS_0 + UCBRF_13 + UCOS16;   // Modln UCBRSx=0, UCBRFx=0,
+	  P4SEL = BIT4+BIT5;                        // P4.5,4 = USCI_A1 TXD/RXD
+	  UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+	  UCA1CTL1 |= UCSSEL_2;                     // SMCLK
+	  UCA1BR0 = 6;                              // 1MHz 9600 (see User's Guide)
+	  UCA1BR1 = 0;                              // 1MHz 9600
+	  UCA1MCTL = UCBRS_0 + UCBRF_13 + UCOS16;   // Modln UCBRSx=0, UCBRFx=0,
 	                                            // over sampling
-	  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-	  UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+	  UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	  UCA1IE |= UCRXIE;                         // Enable USCI_A1 RX interrupt
 
 	  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
 	  __no_operation();                         // For debugger
 }
-#pragma vector=USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
 {
-  switch(__even_in_range(UCA0IV,4))
+  switch(__even_in_range(UCA1IV,4))
   {
   case 0:break;                             // Vector 0 - no interrupt
   case 2:                                   // Vector 2 - RXIFG
-    while (!(UCA0IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
-    UCA0TXBUF = UCA0RXBUF;                  // TX -> RXed character
+    while (!(UCA1IFG&UCTXIFG));             // USCI_A1 TX buffer ready?
+    UCA1TXBUF = UCA1RXBUF;                  // TX -> RXed character
     break;
   case 4:break;                             // Vector 4 - TXIFG
   default: break;
