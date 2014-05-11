4f9a0118d4220db90add956f67ae2553f6fc9011
hshdndx <hanshuang_dndx@163.com>
2014-05-11 22:24:11 +0800

    Add main.c

--- main.c
+++ main.c
@@ -6,7 +6,10 @@
 #include "HAL_Dogs102x6.h"
 #include "config.h"
 #include <stdio.h>
-//using namespace std;
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 
 #define uchar unsigned char
 #define uint unsigned int
@@ -24,19 +27,22 @@
 //unsigned char table[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
 //unsigned char table1[] = {0xbb,0x09,0xea,0x6b,0x59,0x73,0xf3,0x0b,0xfb,0x7b};
 
+//#define p 40.96;
 
-volatile unsigned int result = 0;
+
+unsigned int result;
+float temperature;
 //volatile float result1 = 0.0;
-char t[6];
+char t[15] ;
 
 void InitLcd(void);
 void InitAll(void);
 void Delay400ms(void);
 void Delay(unsigned char i);
 void StartAD(void);
-void SendtoBluetooth(void);
-void SendtoMobile();
-void Sounds(unsigned int);
+//void SendtoBluetooth(void);
+
+void Sounds(char*);
 void Delay1s(void);
 void ADC12Init(void);
 void UARTInit(void);
@@ -56,10 +62,7 @@ void main(void) {
 //    Sounds(result);
 //    unsigned int i;
 //    for(i=0;i<4;i++){
-/*     char t[6];
-     sprintf(t,"%d",result);
-     InitLcd();
-     Dogs102x6_stringDraw(3, 0, t, DOGS102x6_DRAW_NORMAL);  */
+
      unsigned int i;
      for(i=0;i<16000;i++){
     	 Delay1s();
@@ -106,11 +109,9 @@ void ADC12Init(void){
 
 	ADC12CTL0 =  ADC12ON + ADC12SHT0_2+ADC12MSC;         // Sampling time, ADC12 on
     ADC12CTL1 |= ADC12SHP + ADC12CONSEQ_0;                     // Use sampling timer
-//    ADC12CTL1 |=  ADC12DIV_7 + ADC12SSEL_1;
-//    ADC12CTL2 =0x20+ADC12SR+ADC12PDIV;
+
 	ADC12MCTL7 = ADC12INCH_7;
-	ADC12CTL0 |= ADC12REF2_5V;                      //选用内部参考电压为2.5V
-    ADC12CTL0 |= ADC12REFON;                        //内部参考电压打开
+
 	ADC12IE = 0x01;
 	P6SEL |= BIT7;
 	ADC12CTL0 |= ADC12ENC;
@@ -122,12 +123,26 @@ void StartAD(void){
 	ADC12Init();
 	_EINT();
 	ADC12CTL0 |= ADC12SC;
-//    while(1);
-//	char t[6];
-	sprintf(t,"%d",result);
+
+	temperature = (double)(result - 2048)/40.96;
+//	sprintf(t,"%d",result);
+	int a;
+	a= floor(temperature);
+	int b;
+	b = a/10 + 48;
+	t[0] = (char)b;
+	int c;
+	c = a%10 + 48;
+	t[1] = (char)c;
+	int d;
+	d = floor((temperature-a)*10) + 48;
+	t[3] = (char)d;
+	t[2] = '.';
+	t[4] = '\0';
 	InitLcd();
 	Dogs102x6_stringDraw(3, 0, t, DOGS102x6_DRAW_NORMAL);
-	SendtoBluetooth();
+//	SendtoBluetooth();
+	Sounds(t);
 }
 #pragma vector = ADC12_VECTOR
 __interrupt void ADC12_ISR(void){
@@ -138,25 +153,14 @@ __interrupt void ADC12_ISR(void){
 	  case  4: break;                           // Vector  4:  ADC timing overflow
 	  case  6:                                  // Vector  6:  ADC12IFG0
 		result = ADC12MEM0;
+
 		ADC12CTL0 |= ADC12SC;
 	  default: break;
 	  }
 }
 
 
-void UARTInit(void){
-	UCA1CTL1 &= ~UCSWRST;
-	UCA1CTL1 = 0x10;
-	UCA1BR0 = 6;                              // 1MHz 9600 (see User's Guide)
-    UCA1BR1 = 0;                              // 1MHz 9600
-    UCA1MCTL = UCBRS_0 + UCBRF_13 + UCOS16;   // Modln UCBRSx=0, UCBRFx=0,
-//	UTCTL1 = 0x10;
-//	ME2 |= 0x20;
-    UCA1IE |= UCTXIE;
-
-}
-
-void SendtoBluetooth(void){
+/*void SendtoBluetooth(void){
 	unsigned int i;
 //	sprintf(t,"%d",result);
 	UARTInit();
@@ -164,7 +168,8 @@ void SendtoBluetooth(void){
 	for(i=0;i<6;i++){
 		UCA1TXBUF = t[i];
 	}
-}
+}  */
+
 
 
 //sounds function
@@ -181,77 +186,275 @@ void Delay1s()
 
 
 //ISD0-P7.0  ISD1-P7.1  ISD2-P7.2  ISD3-P7.3
-void Sounds(unsigned int temp)
-{
-//	uchar point;
-//	uchar tmp;
-	uchar str[3];
-	str[0]= temp/100;
-	str[1]= temp%100/10;
-	str[2]= temp%10;
-	str[3]=0;
-//	P7DIR |= 0x07;                            // P7.7 output
-	P7OUT |= BIT7;
-	switch(str[0]){
-	case 1: P7OUT |= BIT3 + BIT7; break;
-	case 2: P7OUT |= BIT2 + BIT7; break;
-	case 3: P7OUT |= BIT2 + BIT3 + BIT7; break;
-	case 4: P7OUT |= BIT1 + BIT7; break;
-	case 5: P7OUT |= BIT1 + BIT3 +BIT7; break;
-	case 6: P7OUT |= BIT1 + BIT2 + BIT7; break;
-	case 7: P7OUT |= BIT1 + BIT2 + BIT3 + BIT7; break;
-	case 8: P7OUT |= BIT0 + BIT7; break;
-	case 9: P7OUT |= BIT0 + BIT3 + BIT7; break;
+
+
+void Sounds(char*a){
+	int i;
+	switch(t[0]){
+		case 1:
+			{
+				P7OUT |= BIT3+BIT1 ;
+				for(i=0;i<100;i++){
+					Delay1s();
+				}
+				P7OUT = 0x0000;
+				break;
+			}
+
+
+		case 2:
+			{
+				P7OUT |= BIT1;
+				for(i=0;i<100;i++){
+				Delay1s();
+			    }
+				P7OUT = 0x0000;
+				break;
+			}
+
+
+		case 3:
+			{
+				P7OUT |= BIT0 + BIT1 ;
+				for(i=0;i<100;i++){
+								Delay1s();
+							    }
+				P7OUT = 0x0000;
+				break;
+			}
+
+
+		case 4:
+			{
+				P7OUT |= BIT2 ;
+				for(i=0;i<100;i++){
+	              Delay1s();
+	             }
+				P7OUT = 0x0000;
+				break;
+			}
+
+
+		case 5:
+			{
+				P7OUT |= BIT0 + BIT2 ;
+				for(i=0;i<100;i++){
+					              Delay1s();
+					             }
+				P7OUT = 0x0000;
+				break;
+			}
+
+
+		default: break;
+		}
+
+	if((t[0]!= 0) && (t[0]!= 1)){
+
+		P7OUT |= BIT1 + BIT3;
+		for(i=0;i<100;i++){
+			Delay1s();
+		}
+	    P7OUT = 0x0000;
+	}else{
+		switch(t[1]){
+		case 1:
+		{
+			P7OUT |= BIT0;
+			for(i=0;i<100;i++){
+						Delay1s();
+					}
+			P7OUT = 0x0000;
+			break;
+		}
+		case 2:
+		{
+			P7OUT |= BIT1;
+			for(i=0;i<100;i++){
+			Delay1s();
+						    }
+			P7OUT = 0x0000;
+			break;
+		}
+		case 3:
+		{
+			P7OUT |= BIT0 + BIT1 ;
+			for(i=0;i<100;i++){
+				Delay1s();
+										    }
+			P7OUT = 0x0000;
+			break;
+
+		}
+		case 4:
+		{
+						P7OUT |= BIT2 ;
+						for(i=0;i<100;i++){
+			              Delay1s();
+			             }
+						P7OUT = 0x0000;
+						break;
+					}
+		case 5:
+		{
+			P7OUT |= BIT0 + BIT2;
+			for(i=0;i<100;i++){
+				Delay1s();
+			}
+			P7OUT = 0x0000;
+			break;
+		}
+		case 6:
+		{
+			P7OUT |= BIT1 + BIT2;
+			for(i=0;i<100;i++){
+							Delay1s();
+						}
+						P7OUT = 0x0000;
+						break;
+		}
+		case 7:
+		{
+			P7OUT |= BIT0 + BIT1 + BIT2;
+			for(i=0;i<100;i++){
+										Delay1s();
+									}
+									P7OUT = 0x0000;
+									break;
+		}
+		case 8:
+		{
+			P7OUT |= BIT3;
+			for(i=0;i<100;i++){
+										Delay1s();
+									}
+									P7OUT = 0x0000;
+									break;
+		}
+		case 9:
+		{
+			P7OUT |= BIT0 + BIT3;
+			for(i=0;i<100;i++){
+										Delay1s();
+									}
+									P7OUT = 0x0000;
+									break;
+		}
+		}
 	}
-    P7OUT &= ~BIT7;
-    Delay1s();
-    P7OUT |= BIT7;
-    if(str[0]!=0) P7OUT |= BIT0 + BIT2 + BIT7;
-    P7OUT &= ~BIT7;
-    Delay1s();
-    P7OUT |= BIT7;
-    switch (str[1]){
-       case 0:P7OUT |= BIT7;break;
-       case 1:P7OUT |= BIT3+BIT7;break;
-       case 2:P7OUT |= BIT2+BIT7;break;
-       case 3:P7OUT |= BIT3+BIT7+BIT2;break;
-       case 4:P7OUT |= BIT1+BIT7;break;
-       case 5:P7OUT |= BIT3+BIT7+BIT1;break;
-       case 6:P7OUT |= BIT1+BIT7+BIT2;break;
-       case 7:P7OUT |= BIT3+BIT7+BIT1+BIT2;break;
-       case 8:P7OUT |= BIT0+BIT7;break;
-       case 9:P7OUT |= BIT3+BIT7+BIT0;break;
-       }
-    P7OUT &= ~BIT7;
-    Delay1s();
-    P7OUT |= BIT7;
-    P7OUT |= BIT3+BIT7+BIT0+BIT2;
-    P7OUT &= ~BIT7;
-    Delay1s();
-    P7OUT |= BIT7;
-    switch (str[2]){
-       case 0:P7OUT |= BIT7;break;
-       case 1:P7OUT |= BIT7+BIT3;break;
-       case 2:P7OUT |= BIT7+BIT2;break;
-       case 3:P7OUT |= BIT7+BIT2+BIT3;break;
-       case 4:P7OUT |= BIT7+BIT1;break;
-       case 5:P7OUT |= BIT7+BIT1+BIT3;break;
-       case 6:P7OUT |= BIT7+BIT1+BIT2;break;
-       case 7:P7OUT |= BIT7+BIT1+BIT2+BIT3;break;
-       case 8:P7OUT |= BIT7+BIT0;break;
-       case 9:P7OUT |= BIT7+BIT0+BIT3;break;
-       }
-    P7OUT &= ~BIT7;
-    Delay1s();
-    P7OUT |= BIT7;
-    P7OUT |= BIT0+BIT1+BIT7;
-    P7OUT &= ~BIT7;
-    Delay1s();
-    P7OUT |= BIT7;
+	    P7OUT |= BIT0 + BIT1 + BIT3;
+	    for(i=0;i<100;i++){
+	    							Delay1s();
+	    						}
+	    						P7OUT = 0x0000;
+	    switch(t[3]){
+
+	    case 0:
+	    {
+            P7OUT |= BIT0 + BIT2 + BIT3;
+            for(i=0;i<100;i++){
+            							Delay1s();
+            						}
+            						P7OUT = 0x0000;
+            						break;
+	    }
+
+	    case 1:
+	    		{
+	    			P7OUT |= BIT0;
+	    			for(i=0;i<100;i++){
+	    						Delay1s();
+	    					}
+	    			P7OUT = 0x0000;
+	    			break;
+	    		}
+	    		case 2:
+	    		{
+	    			P7OUT |= BIT1;
+	    			for(i=0;i<100;i++){
+	    			Delay1s();
+	    						    }
+	    			P7OUT = 0x0000;
+	    			break;
+	    		}
+	    		case 3:
+	    		{
+	    			P7OUT |= BIT0 + BIT1 ;
+	    			for(i=0;i<100;i++){
+	    				Delay1s();
+	    										    }
+	    			P7OUT = 0x0000;
+	    			break;
+
+	    		}
+	    		case 4:
+	    		{
+	    						P7OUT |= BIT2 ;
+	    						for(i=0;i<100;i++){
+	    			              Delay1s();
+	    			             }
+	    						P7OUT = 0x0000;
+	    						break;
+	    					}
+	    		case 5:
+	    		{
+	    			P7OUT |= BIT0 + BIT2;
+	    			for(i=0;i<100;i++){
+	    				Delay1s();
+	    			}
+	    			P7OUT = 0x0000;
+	    			break;
+	    		}
+	    		case 6:
+	    		{
+	    			P7OUT |= BIT1 + BIT2;
+	    			for(i=0;i<100;i++){
+	    							Delay1s();
+	    						}
+	    						P7OUT = 0x0000;
+	    						break;
+	    		}
+	    		case 7:
+	    		{
+	    			P7OUT |= BIT0 + BIT1 + BIT2;
+	    			for(i=0;i<100;i++){
+	    										Delay1s();
+	    									}
+	    									P7OUT = 0x0000;
+	    									break;
+	    		}
+	    		case 8:
+	    		{
+	    			P7OUT |= BIT3;
+	    			for(i=0;i<100;i++){
+	    										Delay1s();
+	    									}
+	    									P7OUT = 0x0000;
+	    									break;
+	    		}
+	    		case 9:
+	    		{
+	    			P7OUT |= BIT0 + BIT3;
+	    			for(i=0;i<100;i++){
+	    										Delay1s();
+	    									}
+	    									P7OUT = 0x0000;
+	    									break;
+	    		}
+
+
+
+
+	    }
+	    P7OUT |= BIT2 + BIT3;
+	    for(i=0;i<100;i++){
+	    	  Delay1s();
+	    	              }
+	    P7OUT = 0x0000;
+
+
 
 }
-
-
 
 
 
